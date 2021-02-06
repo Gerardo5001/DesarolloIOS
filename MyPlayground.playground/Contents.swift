0af0fe5c705c4ceb356341e02f7d0b7272838f15
gerardo <al02896769@tecmilenio.mx>
2021-02-05 19:57:13 -0500

    Primer commit

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,8 @@
+import UIKit
+
+var v1 = "esto es un swift"
+let c1 = 4
+var cadena:String = String()
+cadena = "Hola mundo " + v1
+print("El contenido es: \(cadena)")
+print("El valor de constante es: \(c1)")
