b8580b7c8ec7d195697fc2984b7a82002b475cc8
gerardo <al02896769@tecmilenio.mx>
2021-03-22 21:37:54 -0400

    Actividad 8

---
+++ Actividad8_GSL_Prototipo/Actividad8_GSL_Prototipo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Actividad8_GSL_Prototipo
+//
+//  Created by user182860 on 3/22/21.
+//  Copyright © 2021 user182860. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Prueba2/Prueba2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Prueba2
+//
+//  Created by user182860 on 3/22/21.
+//  Copyright © 2021 user182860. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
