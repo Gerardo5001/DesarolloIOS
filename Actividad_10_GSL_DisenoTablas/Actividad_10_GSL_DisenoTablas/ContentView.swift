ca550adc622c657067c91a9a2e47ffd96990946a
gerardo <al02896769@tecmilenio.mx>
2021-04-12 21:23:38 -0400

    Actividad 10

---
+++ Actividad_10_GSL_DisenoTablas/Actividad_10_GSL_DisenoTablas/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Actividad_10_GSL_DisenoTablas
+//
+//  Created by user182860 on 4/12/21.
+//  Copyright © 2021 user182860. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
