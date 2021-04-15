56135e8a353a801541f272f1193b805845132841
gerardo <al02896769@tecmilenio.mx>
2021-04-14 20:32:51 -0400

    Avance evidencia

---
+++ Evidencia_1.0/Evidencia_1.0/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Evidencia_1.0
+//
+//  Created by user182860 on 4/13/21.
+//  Copyright © 2021 user182860. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
